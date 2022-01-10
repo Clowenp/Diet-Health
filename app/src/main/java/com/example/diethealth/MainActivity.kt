22fd7c44c98ce1c80e1b91f66caf47f91fbc8ff8
Clowenp <74609114+Clowenp@users.noreply.github.com>
2022-01-09 22:17:23 -0500

    Created the activities for each page, as well as default buttons to navigate between each page. Basic Headers included as well

--- app/src/main/java/com/example/diethealth/MainActivity.kt
+++ app/src/main/java/com/example/diethealth/MainActivity.kt
@@ -4,11 +4,35 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // navigation bar and main page
+        val navigationBarHome = findViewById<BottomNavigationItemView>(R.id.home_navigation)
+        navigationBarHome.setOnClickListener{
+            val Intent = Intent(this, MainActivity::class.java)
+            startActivity(Intent)
+        }
+        val navigationBarCalendar = findViewById<BottomNavigationItemView>(R.id.calendar_navigation)
+        navigationBarCalendar.setOnClickListener{
+            val Intent = Intent(this, Calendar::class.java)
+            startActivity(Intent)
+        }
+        val navigationBarRecipes = findViewById<BottomNavigationItemView>(R.id.recipes_navigation)
+        navigationBarRecipes.setOnClickListener{
+            val Intent = Intent(this, Recipes::class.java)
+            startActivity(Intent)
+        }
+        val navigationBarProgress = findViewById<BottomNavigationItemView>(R.id.progress_navigation)
+        navigationBarProgress.setOnClickListener{
+            val Intent = Intent(this, Progress::class.java)
+            startActivity(Intent)
+        }
+
 
         //Default Buttons
         val homeActivityButton = findViewById<Button>(R.id.home_button)
