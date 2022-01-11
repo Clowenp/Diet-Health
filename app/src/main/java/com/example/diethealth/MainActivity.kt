01d5883efae7b880875dfa5389d1693497a630bf
16477 <joshua.hill098765@gmail.com>
2022-01-11 10:29:00 -0500

    testing out new code for adding user to database

--- app/src/main/java/com/example/diethealth/MainActivity.kt
+++ app/src/main/java/com/example/diethealth/MainActivity.kt
@@ -9,11 +9,15 @@ import com.example.diethealth.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationItemView
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,8 +51,10 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
         //database variables
-        var database = FirebaseDatabase.getInstance()
-        var dataReference = database.getReference("userInfo")
+        //var database = FirebaseDatabase.getInstance()
+
+        database = Firebase.database.reference
+       //var dataReference = database.getReference("userInfo")
 
         var user = User()
         val genders = resources.getStringArray(R.array.gender_array)
@@ -82,7 +88,8 @@ class MainActivity : AppCompatActivity() {
                 }
                 binding.testTextview.text = user.gender.toString()
                 //adding user to database
-                dataReference.child(user.name).push().setValue(user)
+                //dataReference.child(user.name).push().setValue(user)
+                database.child("test").child(user.name).setValue(user)
             } else {
                 Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
             }
